3c9c197b7ff1ca6c3bca6d188b17ba807ea378a5
Luan <luan.sergio@novahaus.com.br>
2020-04-19 16:26:20 +0000

    Hero gallery structure structure optimization and gallery desktop style

--- src/assets/js/modules/createHeroesGallery.js
+++ src/assets/js/modules/createHeroesGallery.js
@@ -2,27 +2,28 @@ import { heroesList } from '../../../data/index'
 import imgs from "../../img/heroes/*.png";
 
 export function createHeroesGallery(){
-  const ctn = document.querySelector('[data-heroes-gallery]');
-  let i = 0;
-  
+
   heroesList.forEach(elm => {
-    ctn.innerHTML += `<div class="c-heroes" data-heroes-container="${elm.id}"></div>`
-    const heroes = document.querySelectorAll('[data-heroes-container]');
+    const ctn = document.querySelector(`[data-group-ctn="${elm.group}"]`);
     
-    elm.heroes.forEach((elm) => {
-      const name = elm.name;
+    ctn.innerHTML += `<div class="c-gallery__heroes" data-heroes-group="${elm.group}" data-heroes-faction="${elm.faction}"></div>`
+    
+    elm.heroes.forEach((hero) => {
+      const name = hero.name;
+
+      const heroes = document.querySelector(`[data-heroes-group="${elm.group}"][data-heroes-faction="${elm.faction}"]`);
+      
       const imgName = 
       name.split(" ")
-      .join("_")
-      .split("'")
-      .join("")
-      .toLowerCase();
+          .join("_")
+          .split("'")
+          .join("")
+          .toLowerCase();
       
       const imgSrc = imgs[imgName];
       
-      heroes[i].insertAdjacentHTML("beforeend", `<img src="${imgSrc}" class="c-heroes__img" alt="${elm.name}" title="${elm.name}"></img>`  );
+      heroes.insertAdjacentHTML("beforeend", `<img src="${imgSrc}" class="c-gallery__heroes-img" alt="${hero.name}" title="${hero.name}"></img>`  );
     })
-    i++;
   });
   
 }
