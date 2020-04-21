de143afaac7cab77d5af54e415720ecf4385f4f9
Luan <luan.sergio@novahaus.com.br>
2020-04-21 09:26:16 +0000

    Background, random button, heroes hover animations and random function initial structure

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,5 @@
 import { createHeroesGallery } from './modules/createHeroesGallery';
+import { randomHeroes } from './modules/randomHeroes';
 
 createHeroesGallery();
+randomHeroes();
--- src/assets/js/modules/createHeroesGallery.js
+++ src/assets/js/modules/createHeroesGallery.js
@@ -4,9 +4,9 @@ import imgs from "../../img/heroes/*.png";
 export function createHeroesGallery(){
 
   heroesList.forEach(elm => {
-    const ctn = document.querySelector(`[data-group-ctn="${elm.group}"]`);
+    const container = document.querySelector(`[data-group-ctn="${elm.group}"]`);
     
-    ctn.innerHTML += `<div class="c-gallery__heroes" data-heroes-group="${elm.group}" data-heroes-faction="${elm.faction}"></div>`
+    container.innerHTML += `<div class="c-gallery__heroes" data-heroes-group="${elm.group}" data-heroes-faction="${elm.faction}"></div>`
     
     elm.heroes.forEach((hero) => {
       const name = hero.name;
@@ -22,7 +22,7 @@ export function createHeroesGallery(){
       
       const imgSrc = imgs[imgName];
       
-      heroes.insertAdjacentHTML("beforeend", `<img src="${imgSrc}" class="c-gallery__heroes-img" alt="${hero.name}" title="${hero.name}"></img>`  );
+      heroes.insertAdjacentHTML("beforeend", `<img src="${imgSrc}" class="c-gallery__heroes-img" alt="${hero.name}" title="${hero.name}"></img>`);
     })
   });
   
