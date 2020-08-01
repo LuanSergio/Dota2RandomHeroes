6c51a740e9519fd32b7cce9c03d8d3a2bae6cae2
Luan <luan.sergio@novahaus.com.br>
2020-08-01 01:49:25 +0000

    perfomance improvement

--- src/assets/js/modules/create/createHeroesGallery.js
+++ src/assets/js/modules/create/createHeroesGallery.js
@@ -3,48 +3,44 @@ import images from "../../../img/heroes/*.webp";
 import stringToImageName from '../../utils/stringToImageName';
 
 export default function createHeroesGallery() {
-  const width = screen.width;
-  
-  if(width > 767) {
-    heroesList.forEach(list => {
-      const container = document.querySelector(`[data-group="${list.group}"]`);
-      
-      container.insertAdjacentHTML(
-        'beforeend', 
-        `<div 
-          class="c-gallery__heroes" 
-          data-heroes-group="${list.group}" 
-          data-heroes-faction="${list.faction}">
-        </div>`
+  heroesList.forEach(list => {
+    const container = document.querySelector(`[data-group="${list.group}"]`);
+    
+    container.insertAdjacentHTML(
+      'beforeend', 
+      `<div 
+        class="c-gallery__heroes" 
+        data-heroes-group="${list.group}" 
+        data-heroes-faction="${list.faction}">
+      </div>`
+    );
+    
+    list.heroes.forEach((hero) => {
+      const name = hero.name;
+
+      const heroes = document.querySelector(
+        `[data-heroes-group="${list.group}"][data-heroes-faction="${list.faction}"]`
       );
       
-      list.heroes.forEach((hero) => {
-        const name = hero.name;
-  
-        const heroes = document.querySelector(
-          `[data-heroes-group="${list.group}"][data-heroes-faction="${list.faction}"]`
-        );
-        
-        const imageName = stringToImageName(name);
-        
-        const imageSource = images[imageName];
-        
-        heroes.insertAdjacentHTML(
-          'beforeend', 
-          `<div 
-            class="c-gallery__hero" 
-            data-hero="${hero.name}"
-            data-selection="">
-              <img 
-                src="${imageSource}" 
-                class="c-gallery__hero-img" 
-                draggable="false"
-                alt="${hero.name}" 
-                title="${hero.name}">
-              </img>
-          </div>`
-        );
-      });
+      const imageName = stringToImageName(name);
+      
+      const imageSource = images[imageName];
+      
+      heroes.insertAdjacentHTML(
+        'beforeend', 
+        `<div 
+          class="c-gallery__hero" 
+          data-hero="${hero.name}"
+          data-selection="">
+            <img 
+              src="${imageSource}" 
+              class="c-gallery__hero-img" 
+              draggable="false"
+              alt="${hero.name}" 
+              title="${hero.name}">
+            </img>
+        </div>`
+      );
     });
-  }
+  });
 }
