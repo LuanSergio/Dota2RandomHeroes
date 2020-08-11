9dad0890e85e955028d1b01962f57035c3c8f1a7
Luan <luan.sergio@novahaus.com.br>
2020-08-11 07:52:29 +0000

    PWA install prompt

--- service-worker.js
+++ service-worker.js
@@ -36,7 +36,7 @@ self.addEventListener('fetch', (evt) => {
       .catch(() => {
         return caches.open(CACHE_NAME)
         .then((cache) => {
-          return cache.match('offline.html');
+          return cache.match('index.html');
         });
     })
   );
--- src/assets/js/install.js
+++ src/assets/js/install.js
@@ -1,20 +1,3 @@
-// let deferredInstallPrompt = null;
-// const installButton = document.getElementById('butInstall');
-// installButton.addEventListener('click', installPWA);
-
-// window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
-
-// function saveBeforeInstallPromptEvent(evt) {
-//   deferredInstallPrompt = evt;
-//   installButton.removeAttribute('hidden');
-// }
-
-// function installPWA(evt) {
-//   deferredInstallPrompt.prompt();
-//   evt.srcElement.setAttribute('hidden', true);
-// }
-////
-
 const screenWidth = screen.width;
 
 if(screenWidth < 1024) {
