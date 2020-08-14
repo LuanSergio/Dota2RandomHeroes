725cd2d4ee587c7b39dc99fe2dd1ee166f848632
Luan <luan.sergio@novahaus.com.br>
2020-08-14 11:57:58 +0000

    install PWA box

--- src/assets/js/install.js
+++ src/assets/js/install.js
@@ -1,16 +1,45 @@
-const screenWidth = screen.width;
+let deferredInstallPrompt = null;
+const installButton = document.querySelector('[data-install-pwa-button]');
+const installBox = document.querySelector('[data-install-box]')
+installButton.addEventListener('click', installPWA);
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
-if(screenWidth < 1024) {
-  let deferredInstallPrompt = null;
 
-  window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+function saveBeforeInstallPromptEvent(evt) {
+  deferredInstallPrompt = evt;
+  installBox.classList.remove('h-hidden');
+}
 
-  function saveBeforeInstallPromptEvent(evt) {
-    deferredInstallPrompt = evt;
-    installPWA();
-  }
+function installPWA(evt) {
+  deferredInstallPrompt.prompt();
+  // Hide the install button, it can't be called twice.
+  installBox.srcElement.setAttribute('hidden', true);
+  // installBox.classList.add('h-hidden');
 
-  function installPWA() {
-    deferredInstallPrompt.prompt();
-  }
+// CODELAB: Log user response to prompt.
+    deferredInstallPrompt.userChoice
+    .then((choice) => {
+      if (choice.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt', choice);
+      } else {
+        console.log('User dismissed the A2HS prompt', choice);
+      }
+      deferredInstallPrompt = null;
+    });
+}
+
+// CODELAB: Add event listener for appinstalled event
+// CODELAB: Add event listener for appinstalled event
+window.addEventListener('appinstalled', logAppInstalled);
+
+/**
+ * Event handler for appinstalled event.
+ *   Log the installation to analytics or save the event somehow.
+ *
+ * @param {Event} evt
+ */
+function logAppInstalled(evt) {
+  // CODELAB: Add code to log the event
+// CODELAB: Add code to log the event
+console.log('Weather App was installed.', evt);
 }
--- src/assets/js/modules/randomSystem/clearRandomContainer.js
+++ src/assets/js/modules/randomSystem/clearRandomContainer.js
@@ -1,5 +1,4 @@
 export default function clearRandomContainer(){
   const randomContainer = document.querySelector('[data-random-hero');
-  // randomContainer.classList.remove('c-random_hero-ctn');
   randomContainer.innerHTML = '';
 }
