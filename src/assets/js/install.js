285f56bbfaa8b2d83cab3718de73870f81de6a6c
Luan <luan.sergio@novahaus.com.br>
2020-08-15 23:17:10 +0000

    PWA adjusts

--- src/assets/js/install.js
+++ src/assets/js/install.js
@@ -1,45 +1,22 @@
 let deferredInstallPrompt = null;
+const screenWidth = screen.width;
 const installButton = document.querySelector('[data-install-pwa-button]');
 const installBox = document.querySelector('[data-install-box]')
-installButton.addEventListener('click', installPWA);
 window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+installButton.addEventListener('click', installPWA);
 
 
 function saveBeforeInstallPromptEvent(evt) {
   deferredInstallPrompt = evt;
-  installBox.classList.remove('h-hidden');
+  if(screenWidth < 767) {
+    installBox.classList.remove('h-hidden');
+  } else {
+    deferredInstallPrompt.prompt();
+  }
 }
 
 function installPWA(evt) {
   deferredInstallPrompt.prompt();
-  // Hide the install button, it can't be called twice.
   installBox.srcElement.setAttribute('hidden', true);
-  // installBox.classList.add('h-hidden');
-
-// CODELAB: Log user response to prompt.
-    deferredInstallPrompt.userChoice
-    .then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt', choice);
-      } else {
-        console.log('User dismissed the A2HS prompt', choice);
-      }
-      deferredInstallPrompt = null;
-    });
-}
-
-// CODELAB: Add event listener for appinstalled event
-// CODELAB: Add event listener for appinstalled event
-window.addEventListener('appinstalled', logAppInstalled);
-
-/**
- * Event handler for appinstalled event.
- *   Log the installation to analytics or save the event somehow.
- *
- * @param {Event} evt
- */
-function logAppInstalled(evt) {
-  // CODELAB: Add code to log the event
-// CODELAB: Add code to log the event
-console.log('Weather App was installed.', evt);
+  installBox.classList.add('h-hidden');
 }
