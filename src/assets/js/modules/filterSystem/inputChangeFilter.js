615d881d487c0eb96e6a7d6bfdd81847456edbb0
Luan <luan.sergio@novahaus.com.br>
2020-08-20 07:28:01 +0000

    fixed filter state when the value is not valid

--- src/assets/js/modules/filterSystem/inputChangeFilter.js
+++ src/assets/js/modules/filterSystem/inputChangeFilter.js
@@ -1,5 +1,7 @@
 import copyArrayElements from '../../utils/copyArrayElements'
 import filterHeroesArray from './filters/filterHeroesArray'
+import returnFilterType from './returnFilterType'
+import showErrorMessage from './errors/showErrorMessage'
 
 export default function inputChangeFilter(heroes, filterTags) {
   const inputs = document.querySelectorAll('[data-filter-input]');
@@ -7,10 +9,15 @@ export default function inputChangeFilter(heroes, filterTags) {
 
   inputs.forEach(input => {
     input.addEventListener('change', () => {
-      const auxFilters = [];
-      copyArrayElements(auxFilters, filterTags);
-      auxFilters.push({value: input.value, status: '-add'});
-      filterHeroesArray(heroes, auxFilters);
+      if(returnFilterType(input.value)) {
+        const auxFilters = [];
+        copyArrayElements(auxFilters, filterTags);
+        auxFilters.push({value: input.value, status: '-add'});
+        filterHeroesArray(heroes, auxFilters);
+
+      } else {
+        showErrorMessage('This is not a valid filter');
+      }
     });
   });
 }
