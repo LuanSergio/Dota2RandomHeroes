52b48bec806716c2a0e3a3edf60610d5a0df6bff
Luan Sergio <luansergiomattos@gmail.com>
2021-03-30 14:04:08 -0300

    refactor: moved data folder to assets

--- src/assets/js/utils/getData.js
+++ src/assets/js/utils/getData.js
@@ -1,13 +1,13 @@
 // export const heroes = require('./radiant-str.json');
-import * as radiantAgi from '../../../data/heroes/radiant_agi.json';
-import * as radiantInt from '../../../data/heroes/radiant_int.json';
-import * as radiantStr from '../../../data/heroes/radiant_str.json';
+import * as radiantAgi from '../../data/heroes/radiant_agi.json';
+import * as radiantInt from '../../data/heroes/radiant_int.json';
+import * as radiantStr from '../../data/heroes/radiant_str.json';
 
-import * as direAgi from '../../../data/heroes/dire_agi.json';
-import * as direInt from '../../../data/heroes/dire_int.json';
-import * as direStr from '../../../data/heroes/dire_str.json';
+import * as direAgi from '../../data/heroes/dire_agi.json';
+import * as direInt from '../../data/heroes/dire_int.json';
+import * as direStr from '../../data/heroes/dire_str.json';
 
-import * as roles from '../../../data/roles/roles.json'
+import * as roles from '../../data/roles/roles.json'
 
 export const heroesList = [
     radiantStr,
