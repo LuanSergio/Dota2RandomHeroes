6429bcea9b482d18fd5fae4737e67103db21162d
Luan <luan.sergio@novahaus.com.br>
2021-11-21 00:53:09 -0300

    refactor: change misspelled word "previus" to "previous"

--- src/assets/js/modules/create/mobileHeroesGallerySlider.js
+++ src/assets/js/modules/create/mobileHeroesGallerySlider.js
@@ -1,16 +1,17 @@
-import changeSlide from '../slide/changeSlide'
+import changeSlide from "../slide/changeSlide";
 
 export default function mobileHeroesGallery() {
-  const groupContainer = document.querySelectorAll('[data-group-container]');
-  const groups = document.querySelectorAll('[data-group]');
+  const groupContainer = document.querySelectorAll("[data-group-container]");
+  const groups = document.querySelectorAll("[data-group]");
 
   changeSlide(groupContainer, 0);
 
-  groups.forEach(group => {
-    group.insertAdjacentHTML('afterbegin', 
-    `<div class="c-gallery_slide-button-container">
-      <button class="c-gallery_slide-button -previus" alt="Previus" data-button-gallery-previus>
-        Previus
+  groups.forEach((group) => {
+    group.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="c-gallery_slide-button-container">
+      <button class="c-gallery_slide-button -previous" alt="Previous" data-button-gallery-previous>
+        Previous
       /button>
       <button class="c-gallery_slide-button -next" alt="Next" data-button-gallery-next>
         Next
@@ -18,25 +19,27 @@ export default function mobileHeroesGallery() {
     </div>`
     );
   });
-  
-  const buttonNext = document.querySelectorAll('[data-button-gallery-next]');
-  const buttonPrevius = document.querySelectorAll('[data-button-gallery-previus]');
+
+  const buttonNext = document.querySelectorAll("[data-button-gallery-next]");
+  const buttonPrevious = document.querySelectorAll(
+    "[data-button-gallery-previous]"
+  );
   let index = 0;
 
-  buttonPrevius.forEach(button => {
-    button.addEventListener('click', () => {
-      index-=1;
-      if(index < 0) {
+  buttonPrevious.forEach((button) => {
+    button.addEventListener("click", () => {
+      index -= 1;
+      if (index < 0) {
         index = groupContainer.length - 1;
       }
       changeSlide(groupContainer, index);
     });
   });
-  
-  buttonNext.forEach(button => {
-    button.addEventListener('click', () => {
-      index+=1;
-      if(index >= groupContainer.length) {
+
+  buttonNext.forEach((button) => {
+    button.addEventListener("click", () => {
+      index += 1;
+      if (index >= groupContainer.length) {
         index = 0;
       }
       changeSlide(groupContainer, index);
