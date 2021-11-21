9901f698909903222c98e4f0a574cbfaf04b8372
Luan <luan.sergio@novahaus.com.br>
2021-11-21 00:49:47 -0300

    fix: fix install pwa prompt

--- src/assets/js/install.js
+++ src/assets/js/install.js
@@ -1,21 +1,22 @@
 let deferredInstallPrompt = null;
 const screenWidth = screen.width;
-const installButton = document.querySelector('[data-install-pwa-button]');
-const installBox = document.querySelector('[data-install-box]')
-window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
-installButton.addEventListener('click', installPWA);
+const installButton = document.querySelector("[data-install-pwa-button]");
+const installBox = document.querySelector("[data-install-box]");
 
+window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
+installButton.addEventListener("click", installPWA);
 
 function saveBeforeInstallPromptEvent(evt) {
-  deferredInstallPrompt = evt;
-  deferredInstallPrompt.prompt();
-  if(screenWidth < 767) {
-    installBox.classList.remove('h-hidden');
+  if (screenWidth < 767) {
+    deferredInstallPrompt = evt;
+    installBox.classList.remove("h-hidden");
+    installBox.setAttribute("aria-hidden", false);
   }
 }
 
 function installPWA(evt) {
   deferredInstallPrompt.prompt();
-  installBox.srcElement.setAttribute('hidden', true);
-  installBox.classList.add('h-hidden');
+  installBox.setAttribute("aria-hidden", true);
+  installBox.setAttribute("hidden", true);
+  installBox.classList.add("h-hidden");
 }
